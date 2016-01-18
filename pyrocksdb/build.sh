ed020331d5c24ff13fdeb4de67571058960ac8eb
John Dennison <dennison.john@gmail.com>
2016-01-18 21:11:55 +0000

    new rocksdb and pyrocksdb recipes

---
+++ pyrocksdb/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:`pwd`/include
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`pwd`
+#export LIBRARY_PATH=${LIBRARY_PATH}:`pwd`
+CPLUS_INCLUDE_PATH=$PREFIX/include $PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
