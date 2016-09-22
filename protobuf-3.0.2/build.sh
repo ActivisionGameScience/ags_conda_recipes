0a8269ead02701687e08fdb20171b78542485022
James Humphrey <leafknode@gmail.com>
2016-09-22 16:58:38 -0600

    added protobuf 3.0.2 recipe (#4)

---
+++ protobuf-3.0.2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sh autogen.sh
+./configure --prefix=$PREFIX
+
+C_INCLUDE_PATH=$PREFIX/include make
+C_INCLUDE_PATH=$PREFIX/include make install
+
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+
+cd python
+C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib python setup.py build --cpp_implementation
+C_INCLUDE_PATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib python setup.py install --cpp_implementation
