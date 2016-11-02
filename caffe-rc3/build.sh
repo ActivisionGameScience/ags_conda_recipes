723695813e671b4351a07fa68c05ce3b0cf9aef1
Spencer Stirling <sstirling@uber.com>
2016-11-01 21:01:04 -0600

    Caffe and LevelDB fixes

--- caffe-rc3/build.sh
+++ caffe-rc3/build.sh
@@ -3,14 +3,26 @@
 mkdir build
 cd build
 ld && cd build
+
+
 if [[ $(uname) == "Darwin" ]]; then
-    # these two lines should be deleted after rc3 because the cmake file has since been fixed
+    # these two lines should be deleted once we move past version rc3 because the cmake file has since been fixed
     mv ../cmake/Modules/FindvecLib.cmake ../cmake/Modules/FindvecLib.cmake.orig
     sed "s/vecLib.h/cblas.h/g" ../cmake/Modules/FindvecLib.cmake.orig > ../cmake/Modules/FindvecLib.cmake
-    cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCPU_ONLY=ON -DCMAKE_OSX_DEPLOYMENT_TARGET="" 
+
+    pythonlib=`find $PREFIX -name "libpython*.dylib"`
+    python_h=`find $PREFIX -name "Python.h"`
+    pythoninc=`dirname $python_h` 
+    cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCPU_ONLY=ON -DCMAKE_OSX_DEPLOYMENT_TARGET="" -DPYTHON_LIBRARY=$pythonlib -DPYTHON_INCLUDE_DIR=$pythoninc
 else
     cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCPU_ONLY=ON
 fi
 make all
 make install
 #make runtest
+
+# python stuff ends up in the wrong spot
+mv $PREFIX/python/caffe $SP_DIR/
+mv $SP_DIR/caffe/_caffe.dylib $SP_DIR/caffe/_caffe.so
+mv $PREFIX/python/*.py* $PREFIX/bin/
+rm $PREFIX/python/requirements.txt; rmdir $PREFIX/python
--- leveldb-1.19/build.sh
+++ leveldb-1.19/build.sh
@@ -6,6 +6,15 @@ make check
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/include
 
+if [[ $(uname) == "Darwin" ]]; then
+    cd out-shared
+    libfile=`ls libleveldb.dylib.*.*`
+    cd ..
+    install_name_tool -id  @rpath/./$libfile out-shared/$libfile 
+else
+    echo "Do we need to relink for Linux?"
+fi
 mv out-shared/libleveldb.* $PREFIX/lib/
+
 mv out-static/*.a $PREFIX/lib/
 mv include/leveldb $PREFIX/include/
