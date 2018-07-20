2523d476584914a3026b24771ff92579ed259df1
John Dennison <dennison.john@gmail.com>
2018-07-20 13:59:08 +0000

    new kafka libs

---
+++ confluent-kafka-0.11.4/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CPPFLAGS="-I$PREFIX/include -L$PREFIX/lib" $PYTHON setup.py install 
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
